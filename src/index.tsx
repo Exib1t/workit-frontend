fd1c95336290c0ad0cf8c7ad66604527ed9d4d32
Dmitriy Maznyak <dmitriy.maznyak@ideus.biz>
2024-01-24 19:26:49 +0200

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,25 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import AppRouter from "./router/AppRouter.tsx";
 import { BrowserRouter } from "react-router-dom";
-import "./assets/styles/common.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme/theme.ts";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import "./assets/styles/common.scss";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLTIObAewsODe38Ojb-RwhY05XW3VyK1Y",
+  authDomain: "workit-2a960.firebaseapp.com",
+  projectId: "workit-2a960",
+  storageBucket: "workit-2a960.appspot.com",
+  messagingSenderId: "739378291395",
+  appId: "1:739378291395:web:e8a87cef5fab22a469f876",
+  measurementId: "G-C1S468C7XG"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
